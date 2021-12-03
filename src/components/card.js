a6b8297d0ce58fba567bcc23c84b117bacbc6ca4
iamleahb <leah.ball@gmail.com>
2021-12-03 01:42:34 -0600

    card component built, 9 tests passing

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,29 @@
 const Card = (article) => {
+
+//instantiating elements
+const cardDiv = document.createElement('div');
+const cardHeadline = document.createElement('div');
+const author = document.createElement('div');
+const imageDiv = document.createElement('div');
+const authImage = document.createElement('img');
+const authName = document.createElement('span');
+
+//set classes and attributes
+cardDiv.classList.add('card');
+cardHeadline.classList.add('headline');
+author.classList.add('author');
+imageDiv.classList.add('img-container')
+cardHeadline.textContent = article.headline;
+authName.textContent = article.authorName;
+authImage.src = article.authorPhoto;
+
+//create structure
+cardDiv.appendChild(cardHeadline);
+cardDiv.appendChild(author);
+author.appendChild(imageDiv);
+author.appendChild(authName);
+return cardDiv;
+
   // TASK 5
   // ---------------------
   // Implement this function, which should return the markup you see below.
@@ -20,6 +45,9 @@ const Card = (article) => {
 }
 
 const cardAppender = (selector) => {
+
+
+
   // TASK 6
   // ---------------------
   // Implement this function that takes a css selector as its only argument.
